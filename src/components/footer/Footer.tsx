c2f25efef4bf12d357e528afb8b03c4c29e09bc1
Jayesh Dhobe <91770652+JayeshDhobe@users.noreply.github.com>
2024-02-06 11:08:56 +0530

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Jayesh</span>
+      <span>© Jayesh Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
